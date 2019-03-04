8e452cdddc47ac38b1da1694e4a8c8bf6a9e82bb
Marlon Marques <marlon.belohd@gmail.com>
2019-03-04 09:57:08 -0300

    Simple Create form

--- SallesWebMvc/Controllers/SellersController.cs
+++ SallesWebMvc/Controllers/SellersController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SallesWebMvc.Services;
+using SallesWebMvc.Models;
 
 namespace SallesWebMvc.Controllers
 {
@@ -20,6 +21,19 @@ namespace SallesWebMvc.Controllers
         {
             var list = _sellerService.FindAll();
             return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
